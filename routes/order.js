0e74d5430bd6b56333eb9c92064f1ed5b7b58361
Avashbhandari01 <avash115@gmail.com>
2024-05-26 00:30:24 +0545

    Confirm order changes

--- routes/order.js
+++ routes/order.js
@@ -75,6 +75,7 @@ orderRouter.put("/admin/confirmOrreject", async (req, res) => {
 
       order.status = "completed";
       order = await order.save();
+      order = await Order.findByIdAndDelete(id);
 
       // Fetch user information
       const user = await User.findById(order.userID);
